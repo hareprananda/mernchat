9f901b7f43271c69c25a864c979e9cf2fa410e65
hareprananda <52982157+hareprananda@users.noreply.github.com>
2021-02-11 10:09:44 +0800

    installing socket and cryptojs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { io } from 'socket.io-client';
+import dotenv from "dotenv";
+import CryptoJS from "crypto-js";
 
+dotenv.config();
 function App() {
+  
+  useEffect(()=> {
+    const socket = io(process.env.REACT_APP_NODESERVER);
+
+    // setInterval(() => {
+      
+    //   socket.emit("sendMessage",({pesan : encrypted}),(response) => {
+    //     console.log(response)
+    //   })
+    // },10000)
+
+    var kuncey =CryptoJS.AES.encrypt("INI adalah pesan","KEY CRYPTO").toString();
+    var str ="ini adalah pesan";
+    var encrypted =CryptoJS.AES.encrypt(JSON.stringify({str}),kuncey).toString();
+
+
+  
+    var decrypted = CryptoJS.AES.decrypt(encrypted,kuncey).toString(CryptoJS.enc.Utf8)
+
+    console.log(decrypted)
+   
+    
+  },[])
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Socket.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import io from "socket.io-client";
+const Socket = () => {
+    var socket = io();
+    socket.on
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Socket
